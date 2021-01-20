458be044e4768e6de10e3a0496dd67128a2db6c4
Andrii Fedorko <andrii.fedorko01@gmail.com>
2021-01-20 22:46:51 +0200

    Continuous integration: make submition for test data automatically

--- src/task1.py
+++ src/task1.py
@@ -1,8 +1,3 @@
-class MoodGuessUseCase:
-    def is_positive(self, param):
-        return 1
-
-
 import pandas as pd
 
 import os
@@ -17,34 +12,61 @@ from sklearn.naive_bayes import MultinomialNB
 
 from sklearn.pipeline import Pipeline
 
+# test.csv
+import numpy as np
+
+class MoodGuessUseCase:
+    def __init__(self):
+        numpy_array = data.to_numpy()
+        docs_test = numpy_array[:, 0]
+        self.mood_test = numpy_array[:, 1].astype('int')
+
+#        X_train, X_test, Y_train, Y_test = train_test_split(
+ #           X, Y, test_size=0.4, random_state=42)
+        # don't split if the given data base is already a split
+
+        text_clf = Pipeline([('vect', CountVectorizer(stop_words='english')),
+                             ('tfidf', TfidfTransformer()),
+                             ('clf', MultinomialNB()),
+                             ])
+        self.text_clf = text_clf.fit(docs_test, self.mood_test)
+
+    def is_positive(self, param):
+        return 1
+
+    def predict(self, docs_test):
+        self.predicted = self.text_clf.predict(docs_test)
+        return self.predicted
+
+    def mean(self):
+        print(np.mean(self.predicted == self.mood_test))
+
+    def loads(self):
+
+        pass
+
 for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
 
 data = pd.read_csv("D:/projects/ds/data-science-hackathon-machine-learning/train.csv",index_col='id')
 
-# test.csv
-import numpy as np
-
-numpy_array = data.to_numpy()
-X = numpy_array[:, 0]
-Y = numpy_array[:, 1]
 
 
-X_train, X_test, Y_train, Y_test = train_test_split(
-     X, Y, test_size=0.4, random_state=42)
- # don't split if the given data base is already a split
+def present(param):
+    submission = pd.read_csv("D:/projects/ds/data-science-hackathon-machine-learning/submission.csv")
+    submission['sentiment'] = param
+    submission.to_csv("E:/dstesttask1/submission.csv", index=False)
 
 
-text_clf = Pipeline([('vect', CountVectorizer(stop_words='english')),
-                     ('tfidf', TfidfTransformer()),
-                     ('clf', MultinomialNB()),
-                     ])
-Y=Y.astype('int')
-text_clf = text_clf.fit(X, Y)
+def control():
+    sdata = pd.read_csv("E:/dstesttask1/test.csv", index_col='id')
+    # test.csv
+    import numpy as np
+    numpy_array = sdata.to_numpy()
+    reviews_test = numpy_array[:, 0] # 0 is reviews
 
+    mood = MoodGuessUseCase()
 
-predicted = text_clf.predict(X)
-print(np.mean(predicted == Y))
-# predicted = text_clf2.predict(X_test)
-# np.mean(predicted == Y_test)
+    mood.predict(reviews_test)
+    present(mood.predicted)
---
+++ test/test_integration.py
@@ -0,0 +1,5 @@
+from src.task1 import control
+
+
+def test_integration_whole_system():
+    control()
