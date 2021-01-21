76ff2174389e1bb35a45b19f287e6a490a8f7cc6
Andrii Fedorko <andrii.fedorko01@gmail.com>
2021-01-21 19:58:40 +0200

    Write automated unit test, to check that Disperse Estimation formula works well with real distances

--- test/test_unit.py
+++ test/test_unit.py
@@ -1,6 +1,6 @@
 from src.task1 import *
 
-mood = MoodGuessUseCase()
+#mood = MoodGuessUseCase()
 
 
 def test_really_short_positive_comment():
@@ -16,5 +16,29 @@ def test_really_short_negative_comment_contains_rubbish():
 
 
 def test_something():
+    import math
+    def square(a):
+        b = a.copy()
+        for i in range(len(b)):
+            b[i] *= b[i]
+        return b
 
-    pass
+    def avg(A):
+        return sum(A) / len(A)
+
+    def de(A):
+        print('avg(',(square(A)), ')=  ',avg(square(A)), '-', avg(A)**2)
+        return math.sqrt(avg(square(A)) - avg(A) ** 2) / avg(A)
+
+    assert de([30, 30]) == 0
+    assert de([29, 30]) != 0
+    # assert de([0, 30]) == 1
+    # assert de([4, 317]) == .9750778816199377
+    # assert de([4, 317, 480])  == 0.7397612136993055
+    # assert de([4, 317, 480, 495, 537, 613, 643, 667, 704])  == 0.4167387645666981
+    # assert de([4, 317, 480, 495, 537, 613, 643, 667, 704, 737]) == 0.4019249037181562
+    assert de([4, 317-4, 480-317, 495-480, 537-495, 613-537, 643-613, 667-643, 704-667, 737-704, 754-737+4]) == 0.5173128473494533
+
+
+
+
