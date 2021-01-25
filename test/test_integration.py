74a37bc8f23776beccdace213d45202a32d5fe28
Andrii Fedorko <andrii.fedorko01@gmail.com>
2021-01-25 21:43:07 +0200

    Add test/test_integration modified

--- test/test_integration.py
+++ test/test_integration.py
@@ -5,7 +5,7 @@ from src.heavy_model import HeavyModel
 from src.presentation import control
 
 
-@pytest.mark.skip("Run this when you want to kill some time")
+#@pytest.mark.skip("Run this when you want to kill some time")
 def test_integration_whole_system():
     train = pd.read_csv("../kaggle/input/train.csv", index_col='id')
 
